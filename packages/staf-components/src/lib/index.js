099708e9f5469e7767238ca2785cf55e528fd13c
kjetil-hoel <kjetilh@egde.no>
2023-09-22 11:20:16 +0200

    explicit exports of components?

--- packages/staf-components/src/lib/index.js
+++ packages/staf-components/src/lib/index.js
@@ -1,1 +1,9 @@
 // Reexport your entry components here
+// @ts-ignore
+import Button from './components/Button.svelte';
+// @ts-ignore
+import Modal from './components/Modal.svelte';
+// @ts-ignore
+import Select from './components/Select.svelte';
+
+export { Button, Modal, Select };
