e19e629aaf1aafcc8abdd2c4b687501cb6202141
kjetil-hoel <kjetilh@egde.no>
2023-09-22 11:28:44 +0200

    fixed path

--- packages/staf-components/src/lib/index.js
+++ packages/staf-components/src/lib/index.js
@@ -1,9 +1,9 @@
 // Reexport your entry components here
 // @ts-ignore
-import Button from './components/Button.svelte';
+import Button from './components/Button/Button.svelte';
 // @ts-ignore
-import Modal from './components/Modal.svelte';
+import Modal from './components/Modal/Modal.svelte';
 // @ts-ignore
-import Select from './components/Select.svelte';
+import Select from './components/Select/Select.svelte';
 
 export { Button, Modal, Select };
