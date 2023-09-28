8c700184ee5491a3d0513d4b7c1206a28413872a
kjetil-hoel <kjetilh@egde.no>
2023-09-28 13:21:53 +0200

    exporting new components

--- packages/staf-components/src/lib/index.js
+++ packages/staf-components/src/lib/index.js
@@ -5,5 +5,13 @@ import Button from './components/Button/Button.svelte';
 import Modal from './components/Modal/Modal.svelte';
 // @ts-ignore
 import Select from './components/Select/Select.svelte';
+// @ts-ignore
+import Link from './components/Link/Link.svelte';
+// @ts-ignore
+import List from './components/List/List.svelte';
+// @ts-ignore
+import Alert from './components/Alert/Alert.svelte';
+// @ts-ignore
+import Paragraph from './components/Typography/Paragraph/Paragraph.svelte';
 
-export { Button, Modal, Select };
+export { Button, Modal, Select, Link, List, Alert, Paragraph };
